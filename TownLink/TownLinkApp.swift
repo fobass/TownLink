7290a80865bb439ea8c4b80e4bb561bf8fe62983
fobass <isabekove@gmail.com>
2025-01-09 17:42:32 +0600

    Initial Commit

---
+++ TownLink/TownLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TownLinkApp.swift
+//  TownLink
+//
+//  Created by Ernist Isabekov on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TownLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
